ddcacd2935d86dabafcf2ee204f7988ce47703b1
VivekHatte <vivek.hatte@gmail.com>
2025-07-10 14:55:24 -0700

    oraganizing player by group

--- main.py
+++ main.py
@@ -12,7 +12,12 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while(True):
@@ -20,8 +25,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        
+        for actor in updatable:
+            actor.update(dt)
+        for actor in drawable:
+            actor.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60)
         dt /= 1000
